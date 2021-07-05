0e281c474a3f532ec4cd2b883fd3e572b207a466
Dmitriy Kudlovich <carnagery@mail.ru>
2021-07-05 16:25:43 +0300

    mobile nav ready

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let sectionLink = document.querySelectorAll(".section__link");
 let arrow = document.querySelectorAll(".link__arrow");
+let mobileMenu = document.querySelector(".mobile-menu");
+let hamburger = document.querySelector(".hamburger");
+let buttonDelete = document.querySelector(".button--delete");
+let mobileMenuLink = document.querySelectorAll(".mobile-menu__link");
 
 console.log(sectionLink);
 
@@ -14,4 +18,18 @@ arrow.forEach((item, index) => {
     item.style.transform = "rotate(180deg)";
     item.style.marginRight = "75px";
   }
+});
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.add("mobile-menu__active");
+});
+
+buttonDelete.addEventListener("click", () => {
+  mobileMenu.classList.remove("mobile-menu__active");
+});
+
+mobileMenuLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu__active");
+  });
 });
